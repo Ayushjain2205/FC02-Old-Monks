08a6cb923e557004b98b258d117c820565a13cba
Ayush Jain <ayushjain2205@gmail.com>
2022-06-10 18:29:44 +0530

    Add frontend Dapp boilerplate

--- Components/Sample.js
+++ Components/Sample.js
@@ -1,22 +1,18 @@
 import React from "react";
-import { useContract, useProvider } from "wagmi";
+import { useContractRead } from "wagmi";
 import CarbonFree from "../artifacts/contracts/CarbonFree.sol/CarbonFree.json";
 
 function Sample() {
-  const provider = useProvider();
-  const contract = useContract({
-    addressOrName: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
-    contractInterface: CarbonFree,
-    signerOrProvider: provider,
-  });
+  const { data, isError, isLoading } = useContractRead(
+    {
+      addressOrName: "0x5fc8d32690cc91d4c39d9d3abcbd16989f875707",
+      contractInterface: CarbonFree,
+    },
+    "name"
+  );
 
-  console.log(contract);
+  console.log(data);
 
-  const getBalance = async () => {
-    let tokenDecimals = await contract.decimals;
-    console.log(tokenDecimals);
-  };
-  getBalance();
   return (
     <div>
       <h1>Hello</h1>
